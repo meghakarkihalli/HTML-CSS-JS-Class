b0273ca7c21343b53c5ad8ccb83be19b1344b6ce
sahanavarma4 <71297526+sahanavarma4@users.noreply.github.com>
2020-11-10 21:46:03 -0600

    longest string

---
+++ sahana/largeststring.js
@@ -0,0 +1,24 @@
+let states = ['california', 'illinios', 'missouri', 'tennesse', 'george', 'michigan', 'colorado', 'nevadaaass'];
+let wholeArr = [];
+
+function longestStrings(arr) {
+    let longest = 0;
+    for(i =0; i < states.length; i++){
+      if(longest < states[i].length){
+        longest = states[i].length;
+      }
+    }
+    for(j =0; j < states.length; j++){
+      if(states[j].length == longest){
+         wholeArr.push(states[j]);
+      }
+    }
+   if(wholeArr.length == 1){
+     return wholeArr[0]
+   }else{
+      return wholeArr
+  }
+}
+
+
+console.log(longestStrings(states));
